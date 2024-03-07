7c31401634958542f752c363ce2189f3ab4fe146
Albin <Albin.Svensson@medieinstitutet.se>
2024-03-07 21:56:58 +0100

    Added database

--- server/server.js
+++ server/server.js
@@ -8,8 +8,24 @@ const app = express();
 app.use(express.json());
 
 app.post("/users", (req, res) => {
-  console.log(req.body);
+  let data = fs.readFileSync("./users.json");
+  let dataJSON = JSON.parse(data);
+  for (let i = 0; i < dataJSON.users.length; i++) {
+    if (dataJSON.users[i].id === req.body.id)
+      throw console.log("Operation Failed! ".red + "User already in system");
+  }
+  dataJSON.users.push(req.body);
+  let writeData = JSON.stringify(dataJSON);
+
+  fs.writeFileSync("./users.json", writeData);
+  console.log(dataJSON);
+  console.log("Operation Success!".green);
+
   res.end();
+});
+
+app.get("/users", (req, res) => {
+  res.send(JSON.parse(fs.readFileSync("./users.json")));
 });
 
 app.listen(3000, () => console.log("Server is up...".blue));
