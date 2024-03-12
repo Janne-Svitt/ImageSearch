29d6be808b47b6a5b78d214de6ddabbcc551939c
Albin <Albin.Svensson@medieinstitutet.se>
2024-03-12 11:08:54 +0100

    Add Router

---
+++ client/Router.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "./pages/Layout";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Booking } from "./pages/Booking";
+import { Menu } from "./pages/Menu";
+import { Admin } from "./pages/Admin";
+import { NotFound } from "./pages/NotFound";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        index: true,
+      },
+      {
+        path: "/fav",
+        element: <Booking />,
+      },
+    ],
+  },
+]);
