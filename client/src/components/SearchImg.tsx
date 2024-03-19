7eb125a490607996ddceb3fb124c447ec6219b19
Albin <Albin.Svensson@medieinstitutet.se>
2024-03-19 17:49:13 +0100

    Added Spellchecking

--- client/src/components/SearchImg.tsx
+++ client/src/components/SearchImg.tsx
@@ -55,6 +55,7 @@ const SearchImg = (props: ISearchImgProps) => {
       }
     );
   }
+
   return (
     <>
       <article className="bg-neutral-900 rounded-sm p-2 shadow-[0_1px_4px_rgba(0,0,0,0.6)]">
@@ -88,7 +89,14 @@ const SearchImg = (props: ISearchImgProps) => {
           />
         )}
 
-        <img src={props.imgData.link} alt="" className="rounded-md" />
+        <img
+          src={props.imgData.link}
+          alt=""
+          className="rounded-md"
+          onError={(e) => {
+            e.currentTarget.src = "../../images/BrokenImagePlaceholder.png";
+          }}
+        />
         <p className="text-stone-700 text-sm text-center">
           {props.imgData.title}
         </p>
